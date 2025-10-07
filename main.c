73d0a5f6bc31d85465a990c0a4fdff659591906d
HADJ SASSI Mahdi <mahdihadjsassi1@gmail.com>
2025-10-07 10:11:28 +0200

    Libération d’un bloc

--- main.c
+++ main.c
@@ -12,8 +12,31 @@ typedef struct HEADER_TAG {
 HEADER *free_list = NULL;
 
 void set_magic_number(HEADER *header, void *ptr) {
-    long *post_magic = (long *)((char *)ptr + header->bloc_size);
+    long *post_magic = (long *) ((char *) ptr + header->bloc_size);
     *post_magic = MAGIC_NUMBER;
+}
+
+static int verify_magic_numbers(HEADER *header, void *ptr) {
+    long *post_magic = (long *) ((char *) ptr + header->bloc_size);
+    if (header->magic_number != MAGIC_NUMBER || *post_magic != MAGIC_NUMBER) {
+        return 0;
+    }
+    return 1;
+}
+
+static void insert_block_sorted(HEADER *header) {
+    if (free_list == NULL || header < free_list) {
+        header->ptr_next = free_list;
+        free_list = header;
+        return;
+    }
+
+    HEADER *current = free_list;
+    while (current->ptr_next && current->ptr_next < header) {
+        current = current->ptr_next;
+    }
+    header->ptr_next = current->ptr_next;
+    current->ptr_next = header;
 }
 
 void *malloc_3is(size_t size) {
@@ -33,6 +56,19 @@ void *malloc_3is(size_t size) {
     return user_ptr;
 }
 
+void free_3is(void *ptr) {
+    if (!ptr) {
+        return;
+    }
+
+    HEADER *header = (HEADER *) ((char *) ptr - sizeof(HEADER));
+
+    if (!verify_magic_numbers(header, ptr)) {
+        return;
+    }
+
+    insert_block_sorted(header);
+}
 
 int main() {
     void *a = malloc_3is(32);
@@ -40,6 +76,13 @@ int main() {
     void *c = malloc_3is(128);
 
     printf("Allocations faites : a=%p, b=%p, c=%p\n", a, b, c);
+
+    free_3is(b);
+
+    if (free_list)
+        printf("Premier bloc libre : %p (taille %zu)\n", (void *)free_list, free_list->bloc_size);
+    else
+        printf("Aucun bloc libre.\n");
 
     return 0;
 }
